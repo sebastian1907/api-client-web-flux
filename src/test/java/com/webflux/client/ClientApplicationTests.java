86e4822867ede6f8c3605189543790ef8bd7c2e0
Sebastian Contreras Reyes <sebastian19071997@hotmail.com>
2023-12-30 23:40:47 -0500

    gitignore + first commit

---
+++ src/test/java/com/webflux/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
